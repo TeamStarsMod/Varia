2c96db472156ed0d8f37c29f64ca23c4e170e846
Neko110923 <neko110923@163.com>
2025-01-14 19:34:37 +0800

    欢迎来到Varia!

---
+++ src/main/java/xyz/article/variaBukkit/VariaBukkit.java
@@ -0,0 +1,17 @@
+package xyz.article.variaBukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VariaBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
